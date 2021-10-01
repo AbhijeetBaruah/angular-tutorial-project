6054b03c3b1878edf9d8a60eb476e381033f2a1c
AbhijeetBaruah <co17302@yahoo.com>
2021-10-01 14:04:31 +0530

    some routing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FollowersComponent } from './followers/followers.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostsComponent } from './posts/posts.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:PostsComponent},
+  {path:'followers',component:FollowersComponent},
+  {path:'profile/:username',component:ProfileComponent},
+  {path:'**',component:PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,19 @@ import {HttpClientModule} from '@angular/common/http';
 import { PostService } from './services/post.service';
 import { DataService } from './services/data.service';
 import { DataResource } from './common/data';
+import { FollowersComponent } from './followers/followers.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    FollowersComponent,
+    ProfileComponent,
+    PageNotFoundComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
